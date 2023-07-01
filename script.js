33b101c83f480d06d5a83fbaf70e130e3cec0148
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-07-01 21:19:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function updateElementText() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const element = document.getElementById("output");
+      element.textContent = "Hello, world!";
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+updateElementText()
+  .then((result) => {
+    console.log(result); // "Hello, world!"
+  })
+  .catch((error) => {
+    console.error(error);
+  });
